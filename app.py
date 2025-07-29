6caf761469c603ab948a50320b38849e296cf4c5
robi1970mkd <RobiRasPelagon@proton.me>
2025-07-29 19:19:54 +0200

    Update app.py

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def home():
     return "✅ PelagonChat е стартуван успешно!"
 
+@app.route("/status")
+def status():
+    return "✅ Агенти се активни и работат!"
+
 if __name__ == "__main__":
     app.run(debug=True)
